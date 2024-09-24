a666b92f184addcb5af17d31aafd3540696f169c
Simone Menichetti <simon3menichetti95@gmail.com>
2024-09-24 15:01:26 +0200

    feat:( add in Models Type name table and method fillable for column)

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    // nome della tabella
+    protected $table = 'types';
+
+    // utilizzo di fillable per assegnare le colonne
+    protected $fillable = [
+        'name',
+        'description',
+    ];
 }
